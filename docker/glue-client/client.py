a535d51e8581f1199db816c7f322c75d066f20af
dongminlee94 <kid33629@gmail.com>
2023-06-16 01:40:22 +0900

    FEAT: add producer & consumer to glue-client

--- docker/glue-client/client.py
+++ docker/glue-client/client.py
@@ -1,17 +1,33 @@
 """Glue client."""
 
 import rpyc
+from kafka import KafkaConsumer, KafkaProducer
 
 
 class GlueClient:
     """Glue client using RPyC SocketStream connection."""
 
     def __init__(self) -> None:
-        self.rpyc_client = rpyc.connect(host="glue-server", port="12345")
+        self.rpyc_client = rpyc.connect(host="localhost", port="12345")
+        self.producer = KafkaProducer(bootstrap_servers="localhost:9092")
+        self.conumer = KafkaConsumer(
+            "iris_data",
+            bootstrap_servers="localhost:9092",
+            auto_offset_reset="earliest",
+        )
 
     def run(self) -> None:
         """Consumes messages from topic_in and send outputs to topic_out."""
-        print(self.rpyc_client.root.pipeline())
+        for message in self.conumer:
+            # schema_in = message.value["schema"]["fields"]
+            data_in = message.value
+            print(f"\ndata_in: {data_in}\n")
+
+            data_out = self.rpyc_client.root.pipeline(data_in)
+            print(f"data_out: {data_out}\n")
+
+            # response = self.producer.send(topic="new_iris_data", value=data_out).get()
+            # print(response)
 
 
 if __name__ == "__main__":
