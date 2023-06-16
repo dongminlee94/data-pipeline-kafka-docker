eabfe07fc3c0d929fc3daa8f8b8329b9521f0cb3
dongminlee94 <kid33629@gmail.com>
2023-06-16 11:33:48 +0900

    FEAT: change kafka-python to confluent-kafka

--- docker/glue-client/client.py
+++ docker/glue-client/client.py
@@ -1,7 +1,10 @@
 """Glue client."""
 
 import rpyc
-from kafka import KafkaConsumer, KafkaProducer
+from confluent_kafka import DeserializingConsumer, SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroDeserializer, AvroSerializer
+from schema import deserializer_schema, serializer_schema
 
 
 class GlueClient:
@@ -9,29 +12,48 @@ class GlueClient:
 
     def __init__(self) -> None:
         self.rpyc_client = rpyc.connect(host="localhost", port="12345")
-        self.producer = KafkaProducer(bootstrap_servers="localhost:9092")
-        self.conumer = KafkaConsumer(
-            "iris_data",
-            bootstrap_servers="localhost:9092",
-            auto_offset_reset="earliest",
+
+        schema_registry_client = SchemaRegistryClient({"url": "http://localhost:8081"})
+
+        avro_deserializer = AvroDeserializer(
+            schema_registry_client=schema_registry_client,
+            schema_str=deserializer_schema,
+        )
+        self.conumer = DeserializingConsumer(
+            {
+                "bootstrap.servers": "localhost:9092",
+                "group.id": "data-pipeline-kafka",
+                "auto.offset.reset": "earliest",
+                "value.deserializer": avro_deserializer,
+            },
+        )
+        self.conumer.subscribe(["iris_data"])
+
+        avro_serializer = AvroSerializer(
+            schema_registry_client=schema_registry_client,
+            schema_str=serializer_schema,
+        )
+        self.producer = SerializingProducer(
+            {
+                "bootstrap.servers": "localhost:9092",
+                "value.serializer": avro_serializer,
+            },
         )
 
     def run(self) -> None:
         """Consumes messages from topic_in and send outputs to topic_out."""
-        for message in self.conumer:
-            # schema_in = message.value["schema"]["fields"]
-            data_in = message.value
-            print(f"\ndata_in: {data_in}\n")
+        while True:
+            message = self.conumer.poll(timeout=1.0)
 
-            data_out = self.rpyc_client.root.pipeline(data_in)
-            print(f"data_out: {data_out}\n")
+            if message is not None:
+                data_in = message.value()
+                print(f"\ndata_in: {data_in}\n")
 
-            response = self.producer.send(topic="new_iris_data", value=data_out).get()
-            print(
-                f"response.topic: {response.topic}\n"
-                f"response.partition: {response.partition}\n"
-                f"response.offset: {response.offset}\n",
-            )
+                data_out = self.rpyc_client.root.pipeline(data_in)
+                print(f"data_out: {data_out}\n")
+
+                self.producer.produce(topic="new_iris_data", value=data_out)
+                self.producer.flush()
 
 
 if __name__ == "__main__":
--- docker/glue-server/server.py
+++ docker/glue-server/server.py
@@ -1,50 +1,26 @@
 """Glue server."""
 
+from typing import Any
+
 import rpyc
-from confluent_kafka.schema_registry import SchemaRegistryClient
-from confluent_kafka.schema_registry.avro import AvroDeserializer, AvroSerializer
-from confluent_kafka.serialization import MessageField, SerializationContext
 from rpyc.utils.server import ThreadedServer
 
 
 class GlueServer(rpyc.Service):
     """Glue server using RPyC Service."""
 
-    def __init__(self) -> None:
-        schema_registry_client = SchemaRegistryClient({"url": "http://localhost:8081"})
-
-        # Decoder
-        self.decoder = AvroDeserializer(
-            schema_registry_client=schema_registry_client,
-            schema_str=schema_registry_client.get_latest_version("iris_data-value").schema.schema_str,
-        )
-        self.decode_ctx = SerializationContext("iris_data", MessageField.VALUE)
-
-        # Encoder
-        self.encoder = AvroSerializer(
-            schema_registry_client=schema_registry_client,
-            schema_str=schema_registry_client.get_latest_version("new_iris_data-value").schema.schema_str,
-        )
-        self.encode_ctx = SerializationContext("new_iris_data", MessageField.VALUE)
-
-    def exposed_pipeline(self, data_in: bytes) -> bytes:
+    def exposed_pipeline(self, data_in: dict[str, Any]) -> dict[str, Any]:
         """Reconstruct data_in into data_out."""
-        if not isinstance(data_in, bytes):
-            raise RuntimeError("data_in must be of type 'bytes'.")
-
-        # Deserialize data to Avro binary
-        data_in = self.decoder(data=data_in, ctx=self.decode_ctx)
+        if not isinstance(data_in, dict):
+            raise RuntimeError("data_in must be of type 'dict'.")
         print(f"\ndata_in: {data_in}\n")
 
         # Reconstruct data
         data_out = data_in
+
+        if not isinstance(data_out, dict):
+            raise RuntimeError("data_out must be of type 'dict'.")
         print(f"data_out: {data_out}\n")
-
-        # Serialize data to Avro binary
-        data_out = self.encoder(obj=data_out, ctx=self.encode_ctx)
-
-        if not isinstance(data_out, bytes):
-            raise RuntimeError("data_out must be of type 'bytes'.")
         return data_out
 
 
