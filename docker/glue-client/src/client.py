a94ea99a4c5b3487569775a8e1ff2d950d25ca86
Dongmin Lee <kid33629@gmail.com>
2023-06-18 21:48:07 +0900

    Feature/add stream glue (#7)
    * FEAT: add packages
    
    * ENH: update docker compose
    
    * CHORE: remove redis in dockerfile of data-generator
    
    * FEAT: add 'make schema' & create new schema to schema registry
    
    * FEAT: add producer & consumer to glue-client
    
    * FEAT: add avro deserializer to glue-server
    
    * FEAT: complete server & client communication
    
    * ENH: remove schmea curl command
    
    * FEAT: add schema script
    
    * FEAT: change kafka-python to confluent-kafka
    
    * ENH: remove kafka-python package
    
    * CHORE: head in more efficient ways
    
    * ENH: update packages and directory pash in Dockerfiles
    
    * ENH: complete a fully automated command

---
+++ docker/glue-client/src/client.py
@@ -0,0 +1,61 @@
+"""Glue client."""
+
+import rpyc
+from confluent_kafka import DeserializingConsumer, SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroDeserializer, AvroSerializer
+from schema import deserializer_schema, serializer_schema
+
+
+class GlueClient:
+    """Glue client using RPyC SocketStream connection."""
+
+    def __init__(self) -> None:
+        self.rpyc_client = rpyc.connect(host="glue-server", port="12345")
+
+        schema_registry_client = SchemaRegistryClient({"url": "http://schema-registry:8081"})
+
+        avro_deserializer = AvroDeserializer(
+            schema_registry_client=schema_registry_client,
+            schema_str=deserializer_schema,
+        )
+        self.consumer = DeserializingConsumer(
+            {
+                "bootstrap.servers": "broker:29092",
+                "group.id": "data-pipeline-kafka",
+                "auto.offset.reset": "earliest",
+                "value.deserializer": avro_deserializer,
+            },
+        )
+        self.consumer.subscribe(["iris_data"])
+
+        avro_serializer = AvroSerializer(
+            schema_registry_client=schema_registry_client,
+            schema_str=serializer_schema,
+        )
+        self.producer = SerializingProducer(
+            {
+                "bootstrap.servers": "broker:29092",
+                "value.serializer": avro_serializer,
+            },
+        )
+
+    def run(self) -> None:
+        """Consumes messages from topic_in and send outputs to topic_out."""
+        while True:
+            message = self.consumer.poll(timeout=1.0)
+
+            if message is not None:
+                data_in = message.value()
+                print(f"\ndata_in: {data_in}\n")
+
+                data_out = self.rpyc_client.root.pipeline(data_in)
+                print(f"data_out: {data_out}\n")
+
+                self.producer.produce(topic="new_iris_data", value=data_out)
+                self.producer.flush()
+
+
+if __name__ == "__main__":
+    glue_client = GlueClient()
+    glue_client.run()
---
+++ docker/glue-client/src/schema.py
@@ -0,0 +1,92 @@
+"""Schema for AvroDeserializer and AvroSerializer."""
+
+deserializer_schema = """
+{
+    "name": "iris_data",
+    "type": "record",
+    "fields": [
+        {
+            "name": "id",
+            "type": ["int", "null"]
+        },
+        {
+            "name": "timestamp",
+            "type": ["string", "null"]
+        },
+        {
+            "name": "sepal_length",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "sepal_width",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "petal_length",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "petal_width",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "target",
+            "type": ["int", "null"]
+        }
+    ]
+}
+"""
+
+
+serializer_schema = """
+{
+    "name": "new_iris_data",
+    "type": "record",
+    "fields": [
+        {
+            "name": "id",
+            "type": ["int", "null"]
+        },
+        {
+            "name": "timestamp",
+            "type": ["string", "null"]
+        },
+        {
+            "name": "sepal_length",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "sepal_width",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "sepal_sum",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "sepal_mean",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "petal_length",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "petal_width",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "petal_sum",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "petal_mean",
+            "type": ["double", "null"]
+        },
+        {
+            "name": "target",
+            "type": ["int", "null"]
+        }
+    ]
+}
+"""
